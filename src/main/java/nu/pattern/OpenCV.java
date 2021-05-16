67291dd7594d32cab51a163b5a89d48b289a125d
Roman Valls Guimera <brainstorm@nopcode.org>
2021-05-16 21:50:11 +1000

    Version bump to OpenCV 4.5.2 and BUILD SUCCESS :tada:

--- src/main/java/nu/pattern/OpenCV.java
+++ src/main/java/nu/pattern/OpenCV.java
@@ -377,17 +377,8 @@ public class OpenCV {
           case X86_64:
             location = "/nu/pattern/opencv/osx/x86_64/libopencv_java453.dylib";
             break;
-//          case AARCH64:
-//            location = "/nu/pattern/opencv/osx/AARCH64/libopencv_java452.dylib";
-//            break;
           case ARMv8:
-            location = "/nu/pattern/opencv/osx/ARMv8/libopencv_java452.dylib";
-            break;
-//          case AARCH64:
-//            location = "/nu/pattern/opencv/osx/AARCH64/libopencv_java451.dylib";
-//            break;
-          case ARMv8:
-            location = "/nu/pattern/opencv/osx/ARMv8/libopencv_java451.dylib";
+            location = "/nu/pattern/opencv/osx/ARMv8/libopencv_java453.dylib";
             break;
           default:
             throw new UnsupportedPlatformException(os, arch);
